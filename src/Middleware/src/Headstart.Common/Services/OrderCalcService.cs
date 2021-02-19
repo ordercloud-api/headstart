4f3f743f11cd0b657f2526b57d9fa7ca3ad2e448
Eric <eking@four51.com>
2021-02-19 14:09:31 -0600

    SEB-1561 ensure PO shipping is not applied to credit card

--- src/Middleware/src/Headstart.Common/Services/OrderCalcService.cs
+++ src/Middleware/src/Headstart.Common/Services/OrderCalcService.cs
@@ -1,4 +1,5 @@
-﻿using Headstart.Common.Services.ShippingIntegration.Models;
+﻿using Headstart.Common.Extensions;
+using Headstart.Common.Services.ShippingIntegration.Models;
 using Headstart.Models;
 using ordercloud.integrations.exchangerates;
 using ordercloud.integrations.library;
@@ -33,10 +34,30 @@ namespace Headstart.Common.Services
 
         public decimal GetPurchaseOrderTotal(HSOrderWorksheet worksheet)
         {
-            return worksheet.LineItems
+            var shippingTotal = GetPurchaseOrderShipping(worksheet);
+            return worksheet.GetLineItemsByProductType(ProductType.PurchaseOrder)
                 .Where(li => li.Product.xp.ProductType == ProductType.PurchaseOrder)
                 .Select(li => li.LineTotal)
-                .Sum();
+                .Sum() + shippingTotal;
+        }
+
+        public decimal GetPurchaseOrderShipping(HSOrderWorksheet worksheet)
+        {
+            decimal POShippingCosts = 0;
+            var POlineItemIDs = worksheet.GetLineItemsByProductType(ProductType.PurchaseOrder)?.Select(li => li.ID);
+            if (POlineItemIDs?.Count() > 0)
+            {
+                foreach (var estimate in worksheet?.ShipEstimateResponse?.ShipEstimates)
+                {
+                    var relatedLineItemIDs = estimate?.ShipEstimateItems?.Select(item => item?.LineItemID);
+                    var selectedShipMethod = estimate?.ShipMethods?.FirstOrDefault(method => method?.ID == estimate?.SelectedShipMethodID);
+                    if (relatedLineItemIDs != null && relatedLineItemIDs.Any(id => POlineItemIDs.Contains(id)))
+                    {
+                        POShippingCosts = POShippingCosts + (selectedShipMethod?.Cost ?? 0);
+                    }
+                }
+            }
+            return POShippingCosts;
         }
     }
 }
