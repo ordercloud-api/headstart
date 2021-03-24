ed491d13cb338330e2e518d1d108938634db6d38
DJ Steinmetz <dsteinmetz@four51.com>
2021-03-24 15:04:28 -0500

    HDS-179: Remove unnecessary code

--- src/UI/Seller/src/app/seller-users/seller-user.service.ts
+++ src/UI/Seller/src/app/seller-users/seller-user.service.ts
@@ -4,8 +4,6 @@ import { User } from '@ordercloud/angular-sdk'
 import { ResourceCrudService } from '@app-seller/shared/services/resource-crud/resource-crud.service'
 import { CurrentUserService } from '@app-seller/shared/services/current-user/current-user.service'
 import { AdminUsers } from 'ordercloud-javascript-sdk'
-import { ToastrService } from 'ngx-toastr'
-import OrderCloudError from 'ordercloud-javascript-sdk/dist/utils/OrderCloudError'
 import { AppErrorHandler } from '@app-seller/config/error-handling.config'
 
 // TODO - this service is only relevent if you're already on the supplier details page. How can we enforce/inidcate that?
@@ -35,19 +33,5 @@ export class SellerUserService extends ResourceCrudService<User> {
     LastName: '',
     Email: '',
     Phone: '',
-  }
-
-  async createNewResource(resource: any): Promise<any> {
-    try {
-      const args = await this.createListArgs([resource])
-      const newResource = await this.ocService.Create(...args)
-      this.resourceSubject.value.Items = [
-        ...this.resourceSubject.value.Items,
-        newResource,
-      ]
-      return newResource
-    } catch (err) {
-      return this.errorHandler.displayError(err)
-    }
   }
 }
