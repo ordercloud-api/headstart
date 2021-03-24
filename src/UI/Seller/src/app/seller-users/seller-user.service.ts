c5ca2c6b404e5d06a145d3af3ba329f879c7450a
DJ Steinmetz <dsteinmetz@four51.com>
2021-03-24 10:29:27 -0500

    HDS-179: Better err messaging for dup username/seller user

--- src/UI/Seller/src/app/seller-users/seller-user.service.ts
+++ src/UI/Seller/src/app/seller-users/seller-user.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core'
 import { Router, ActivatedRoute } from '@angular/router'
-import { User, OcAdminUserService } from '@ordercloud/angular-sdk'
+import { User } from '@ordercloud/angular-sdk'
 import { ResourceCrudService } from '@app-seller/shared/services/resource-crud/resource-crud.service'
 import { CurrentUserService } from '@app-seller/shared/services/current-user/current-user.service'
 import { AdminUsers } from 'ordercloud-javascript-sdk'
+import { ToastrService } from 'ngx-toastr'
+import OrderCloudError from 'ordercloud-javascript-sdk/dist/utils/OrderCloudError'
 
 // TODO - this service is only relevent if you're already on the supplier details page. How can we enforce/inidcate that?
 @Injectable({
@@ -13,7 +15,8 @@ export class SellerUserService extends ResourceCrudService<User> {
   constructor(
     router: Router,
     activatedRoute: ActivatedRoute,
-    currentUserService: CurrentUserService
+    currentUserService: CurrentUserService,
+    private toastrService: ToastrService
   ) {
     super(
       router,
@@ -31,5 +34,24 @@ export class SellerUserService extends ResourceCrudService<User> {
     LastName: '',
     Email: '',
     Phone: '',
+  }
+
+  async createNewResource(resource: any): Promise<any> {
+    try {
+      const args = await this.createListArgs([resource])
+      const newResource = await this.ocService.Create(...args)
+      this.resourceSubject.value.Items = [
+        ...this.resourceSubject.value.Items,
+        newResource,
+      ]
+      this.resourceSubject.next(this.resourceSubject.value)
+      return newResource
+    } catch (err) {
+      this.toastrService.error(
+        err?.errors?.Errors?.[0]?.Message,
+        (err as OrderCloudError)?.statusText
+      )
+      return
+    }
   }
 }
