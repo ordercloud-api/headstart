738aca798dcf55ac63aad07b4d5ff8218fe5a454
Mike Moebeck <mmoebeck@four51.com>
2021-04-16 15:30:28 -0500

    HDS-268 - Seed Update

--- src/Middleware/src/Headstart.API/Commands/EnvironmentSeed/SeedConstants.cs
+++ src/Middleware/src/Headstart.API/Commands/EnvironmentSeed/SeedConstants.cs
@@ -88,7 +88,8 @@ namespace Headstart.API.Commands
         public static readonly List<Incrementor> DefaultIncrementors = new List<Incrementor>() {
             new Incrementor { ID = "orderIncrementor", Name = "Order Incrementor", LastNumber = 0, LeftPaddingCount = 6 },
             new Incrementor { ID = "supplierIncrementor", Name = "Supplier Incrementor", LastNumber = 0, LeftPaddingCount = 3 },
-            new Incrementor { ID = "buyerIncrementor", Name = "Buyer Incrementor", LastNumber = 0, LeftPaddingCount = 4 }
+            new Incrementor { ID = "buyerIncrementor", Name = "Buyer Incrementor", LastNumber = 0, LeftPaddingCount = 4 },
+            new Incrementor { ID = "sellerLocationIncrementor", Name = "Seller Location Incrementor", LastNumber = 0, LeftPaddingCount = 4 }
         };
 
         #region API CLIENTS
