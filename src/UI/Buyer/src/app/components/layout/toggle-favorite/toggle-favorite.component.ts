1500b70a28f41cc0d43f56b99cd6903c1a6b7369
Eric <eking@four51.com>
2021-04-20 16:12:51 -0500

    HDS-267 dont show favorite products toggle for anon users

--- src/UI/Buyer/src/app/components/layout/toggle-favorite/toggle-favorite.component.ts
+++ src/UI/Buyer/src/app/components/layout/toggle-favorite/toggle-favorite.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core'
 import { faHeart as _faHeartFilled } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as _faHeartOutline } from '@fortawesome/free-regular-svg-icons'
+import { ShopperContextService } from 'src/app/services/shopper-context/shopper-context.service'
 
 @Component({
   templateUrl: './toggle-favorite.component.html',
   styleUrls: ['./toggle-favorite.component.scss'],
 })
-export class OCMToggleFavorite {
+export class OCMToggleFavorite implements OnInit {
   faHeartFilled = _faHeartFilled
   faHeartOutline = _faHeartOutline
   @Input() favorite: boolean
   @Output() favoriteChanged = new EventEmitter<boolean>()
+  isAnonymous: boolean
+
+  constructor(
+    private context: ShopperContextService
+  ){ }
+
+  ngOnInit() {
+    this.isAnonymous = this.context.currentUser.isAnonymous()
+  }
 }
