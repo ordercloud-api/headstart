8a9220832218d5456d801bee13616eeee9fefc04
Ryan Lauer <rlauer@four51.com>
2021-05-10 10:02:52 -0500

    HDS-328 - Remove AccessToken to call non-seed endpoint

--- src/Middleware/src/Headstart.API/Controllers/BuyerLocationController.cs
+++ src/Middleware/src/Headstart.API/Controllers/BuyerLocationController.cs
@@ -51,7 +51,7 @@ namespace Headstart.Common.Controllers
         [HttpPut, Route("{buyerID}/{buyerLocationID}"), OrderCloudUserAuth(ApiRole.UserGroupAdmin, ApiRole.AddressAdmin)]
         public async Task<HSBuyerLocation> Save(string buyerID, string buyerLocationID, [FromBody] HSBuyerLocation buyerLocation)
         {
-            return await _buyerLocationCommand.Save(buyerID, buyerLocationID, buyerLocation, UserContext.AccessToken, _oc);
+            return await _buyerLocationCommand.Save(buyerID, buyerLocationID, buyerLocation);
         }
 
         [DocName("Delete a Buyer Location")]
