2d4524ea5e3524d9a2f6626f7b9709371562ef0d
Crhistian Ramirez <cramirez@four51.com>
2021-05-14 15:42:52 -0500

    SEB-1825 only try to remove prmo once per order

--- src/Middleware/src/Headstart.API/Commands/PromotionCommand.cs
+++ src/Middleware/src/Headstart.API/Commands/PromotionCommand.cs
@@ -1,11 +1,12 @@
 using Flurl.Http;
 using Headstart.Common;
 using ordercloud.integrations.library;
-using OrderCloud.Catalyst;
 using OrderCloud.SDK;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using OrderCloud.Catalyst;
 
 namespace Headstart.API.Commands
 {
@@ -17,49 +18,75 @@ namespace Headstart.API.Commands
     public class PromotionCommand : IPromotionCommand
     {
         private readonly IOrderCloudClient _oc;
-        private readonly AppSettings _settings;
-        public PromotionCommand(IOrderCloudClient oc, AppSettings settings)
+        public PromotionCommand(IOrderCloudClient oc)
         {
             _oc = oc;
-            _settings = settings;
         }
 
         public async Task AutoApplyPromotions(string orderID)
         {
-            try
-            {
-                await _oc.Orders.ValidateAsync(OrderDirection.Incoming, orderID);
-            }
-            catch (Exception ex)
-            {
-                await RemoveOrderPromotions(orderID);
-            }
-
-            var ocAuth = await _oc.AuthenticateAsync();
-
-            var autoEligablePromos = await _oc.Promotions.ListAsync(filters: "xp.Automatic=true");
-            await Throttler.RunAsync(autoEligablePromos.Items, 100, 5, promo =>
-            {
-                //  Not useing the sdk here because we need to ignore errors on promos that are not able to be 
-                //  applied to the order. We need to try this request on every automatic promo.
-                return $"{_settings.OrderCloudSettings.ApiUrl}/v1/orders/Incoming/{orderID}/promotions/{promo.Code}"
-                    .WithOAuthBearerToken(ocAuth.AccessToken)
-                    .AllowAnyHttpStatus().PostAsync(null);
-            });
+            await RemoveAllPromotionsAsync(orderID);
+            var autoEligiblePromos = await _oc.Promotions.ListAsync(filters: "xp.Automatic=true");
+            var requests = autoEligiblePromos.Items.Select(p => TryApplyPromoAsync(orderID, p.Code));
+            await Task.WhenAll(requests);
         }
 
-        private async Task RemoveOrderPromotions(string orderID)
+        private async Task RemoveAllPromotionsAsync(string orderID)
         {
-            var curPromos = await _oc.Orders.ListPromotionsAsync(OrderDirection.Incoming, orderID);
-            if(curPromos?.Items?.Count > 0)
+            // ordercloud does not re-evaluate promotions when line items change
+            // we must remove all promos and re-apply them to ensure promotion discounts are accurate
+            var promos = await _oc.Orders.ListPromotionsAsync(OrderDirection.Incoming, orderID, pageSize: 100);
+            var requests = promos.Items
+                            .DistinctBy(p => p.ID) // the same promo may be applied to multiple line items on one order
+                            .Select(p => RemovePromoAsync(orderID, p));
+            var allTasks = Task.WhenAll(requests);
+
+            try
             {
-                var removeQueue = new List<Task>();
-
-                await Throttler.RunAsync(curPromos.Items, 100, 5, promo =>
+                await allTasks;
+            }
+            catch (Exception)
+            {
+                throw new CatalystBaseException(new ApiError
                 {
-                    return _oc.Orders.RemovePromotionAsync(OrderDirection.Incoming, orderID, promo.Code);
-
+                    ErrorCode = "Promotion.ErrorRemovingAll",
+                    Message = "One or more promotions could not be removed",
+                    Data = allTasks.Exception.InnerExceptions
                 });
+            }
+        }
+
+        private async Task<Order> RemovePromoAsync(string orderID, OrderPromotion promo)
+        {
+            try
+            {
+                return await _oc.Orders.RemovePromotionAsync(OrderDirection.Incoming, orderID, promo.Code);
+            }
+            catch
+            {
+                // this can leave us in a bad state if the promotion can't be deleted but no longer valid
+                throw new CatalystBaseException(new ApiError
+                {
+                    ErrorCode = "Promotion.ErrorRemoving",
+                    Message = $"Unable to remove promotion {promo.Code}",
+                    Data = new
+                    {
+                        Promotion = promo
+                    }
+                });
+            }
+        }
+
+        private async Task TryApplyPromoAsync(string orderID, string promoCode)
+        {
+            try
+            {
+                await _oc.Orders.AddPromotionAsync(OrderDirection.Incoming, orderID, promoCode);
+            }
+            catch
+            {
+                // Its not possible to know if a promo is valid without trying to add it
+                // if it isn't valid it shouldn't show up as an error to the user
             }
         }
     }
---
+++ src/Middleware/tests/Headstart.Tests/TestExtensions.cs
@@ -0,0 +1,42 @@
+﻿using NSubstitute.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Headstart.Tests
+{
+    public static class TestExtensions
+    {
+        public static Task<T> ToTask<T>(this T self)
+        {
+            return Task.FromResult(self);
+        }
+
+        // use this on .Returns when you want to return the first argument
+        // passed to the calling function for example on Saves or Creates generally
+        public static Task<T> FirstArgument<T>(this CallInfo args)
+        {
+            return args.ArgAt<T>(0).ToTask();
+        }
+
+        // pick a single random item out of a list
+        public static T PickRandom<T>(this IEnumerable<T> source)
+        {
+            return source.PickRandom(1).Single();
+        }
+
+        // pick count number of items out of list
+        public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count)
+        {
+            return source.Shuffle().Take(count);
+        }
+
+        // re-arrange items in a list
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.OrderBy(x => Guid.NewGuid());
+        }
+    }
+}
