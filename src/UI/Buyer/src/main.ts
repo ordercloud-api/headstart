b66193810c03b2ef281a584bb796ccdb213bba43
Eric King <eking@sitecore.com>
2021-07-07 15:15:12 -0500

    HDS-464 adjust how we check the pathname

--- src/UI/Buyer/src/main.ts
+++ src/UI/Buyer/src/main.ts
@@ -9,7 +9,8 @@ declare var __webpack_public_path__: string;
 //  set the __webpack_public_path__ depending on if the first path after the base url is the storefront name.
 //  Do this for automatic deployments in which files are copied to a folder in blob storage named the storefront name
 //  This allows assets to be referenced correctly.
-if (window.location.href.split('/')[1] === environment.storefrontName) {
+if (document.location.pathname.split('/')[1] === environment.storefrontName) {
+  console.log("setting __webpack_public_path__")
   __webpack_public_path__ = environment.storefrontName
 }
 
