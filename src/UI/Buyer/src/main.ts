b5b538f3f1f2ef0849543dca3615e7cdf6442722
Eric King <eking@sitecore.com>
2021-07-14 09:59:46 -0500

    HDS-464 remove bad code from merge

--- src/UI/Buyer/src/main.ts
+++ src/UI/Buyer/src/main.ts
@@ -23,15 +23,6 @@ if (document.location.pathname.split('/')[1] === environment.storefrontName) {
 
 declare var __webpack_public_path__: string;
 
-//  set the __webpack_public_path__ depending on if the first path after the base url is the storefront name.
-//  Do this for automatic deployments in which files are copied to a folder in blob storage named the storefront name
-//  This allows assets to be referenced correctly.
-console.log('pathname: ' + document.location.pathname)
-console.log('window.location: ' + window.location.href)
-if (document.location.pathname.split('/')[1] === environment.storefrontName) {
-  console.log("setting __webpack_public_path__")
-  __webpack_public_path__ = environment.storefrontName
-}
 
 if (environment.hostedApp) {
   enableProdMode()
