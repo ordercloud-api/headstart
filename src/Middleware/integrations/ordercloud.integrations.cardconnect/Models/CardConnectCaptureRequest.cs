47d1f890906becc395995b8ce889352b0ae97a22
Crhistian Ramirez <cramirez@four51.com>
2021-09-16 14:18:56 -0500

    resolve differences

---
+++ src/Middleware/integrations/ordercloud.integrations.cardconnect/Models/CardConnectCaptureRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ordercloud.integrations.cardconnect
+{
+    public class CardConnectCaptureRequest
+    {
+        // https://developer.cardconnect.com/cardconnect-api#capture-request
+        public string merchid { get; set; } // required
+        public string retref { get; set; } // required
+        public string currency { get; set; } // required
+        public string authcode { get; set; }
+        public string amount { get; set; }
+        public string invoiceid { get; set; }
+        public string receipt { get; set; }
+    }
+
+    public class CardConnectCaptureResponse
+    {
+        // https://developer.cardconnect.com/cardconnect-api#capture-response
+        public string merchid { get; set; }
+        public string account { get; set; }
+        public string orderId { get; set; }
+        public string amount { get; set; }
+        public string retref { get; set; }
+        public string batchid { get; set; }
+        public string setlstat { get; set; }
+        public dynamic receipt { get; set; }
+        public string respstat { get; set; }
+        public string resptext { get; set; }
+        public string respcode { get; set; }
+    }
+}
---
+++ src/Middleware/src/Headstart.Common/Repositories/Models/ProductDetailInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Headstart.Common.Repositories.Models
+{
+    public class ProductDetailInfo
+    {
+        public decimal LineTotal { get; set; }
+        public string ProductID { get; set; }
+        public double Quantity { get; set; }
+        public string Name { get; set; }
+    }
+}
