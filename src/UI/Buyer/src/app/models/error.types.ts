37c7da08a974ae9c036f29b116413b5d5041ecfd
Crhistian Ramirez <cramirez@four51.com>
2021-09-17 07:51:27 -0500

    fix: errors not displaying correctly due to change in error structure

--- src/UI/Buyer/src/app/models/error.types.ts
+++ src/UI/Buyer/src/app/models/error.types.ts
@@ -1,24 +1,24 @@
 export interface MiddlewareError<TData = any> {
-    ErrorCode: string
-    Message: string
-    Data: TData
-  }
-  
-  export interface InventoryErrorData {
-    LineItemID: string
-    ProductID: string
-    QuantityAvailable: number
-    QuantityRequested: number
-    VariantID: string
-  }
+  ErrorCode: string
+  Message: string
+  Data: TData
+}
 
-  export interface ErrorDisplayData {
-    code?: string
-    title?: string
-    message?: string
-    buttonText?: string
-  }
-  
-  export interface ErrorTypes {
-    [key: string]: ErrorDisplayData
-  }
+export interface InventoryErrorData {
+  LineItemID: string
+  ProductID: string
+  QuantityAvailable: number
+  QuantityRequested: number
+  VariantID: string
+}
+
+export interface ErrorDisplayData {
+  code?: string
+  title?: string
+  message?: string
+  buttonText?: string
+}
+
+export interface ErrorTypes {
+  [key: string]: ErrorDisplayData
+}
