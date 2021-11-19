5e66b1c391706b82f7b3d15ee08c00b592f0b945
Oliver Heywood <oliver.heywood@sitecore.com>
2021-11-19 16:46:49 -0600

    trying to get product links working

--- src/UI/Buyer/src/app/components/layout/preview-search/preview-search.component.ts
+++ src/UI/Buyer/src/app/components/layout/preview-search/preview-search.component.ts
@@ -4,6 +4,7 @@ import {
   OnInit,
 } from '@angular/core'
 import { ReflektionSearchResponse } from 'src/app/services/reflektion/models/ReflektionSearchResponse'
+import { ShopperContextService } from 'src/app/services/shopper-context/shopper-context.service';
 
 @Component({
   templateUrl: './preview-search.component.html',
@@ -13,7 +14,16 @@ export class OCMPreviewSearch implements OnInit {
   @Input() searchTerm?: string;
   @Input() results?: ReflektionSearchResponse;
 
+  constructor(
+    public context: ShopperContextService,
+  ) {
+  }
+
   ngOnInit(): void {
+  }
+
+  toProductDetails(productID: string): void {
+    this.context.router.toProductDetails(productID);
   }
 
 }
