02468d5363d915d8fbfa8fe3dbc06989619f0264
Oliver Heywood <oliver.heywood@sitecore.com>
2021-11-22 10:06:08 -0600

    hover on suggestions shows products

--- src/UI/Buyer/src/app/components/layout/preview-search/preview-search.component.ts
+++ src/UI/Buyer/src/app/components/layout/preview-search/preview-search.component.ts
@@ -6,6 +6,7 @@ import {
   EventEmitter
 } from '@angular/core'
 import { ReflektionSearchResponse } from 'src/app/services/reflektion/models/ReflektionSearchResponse'
+import { ReflektionService } from 'src/app/services/reflektion/reflektion.service';
 import { ShopperContextService } from 'src/app/services/shopper-context/shopper-context.service';
 
 @Component({
@@ -19,6 +20,7 @@ export class OCMPreviewSearch implements OnInit {
 
   constructor(
     public context: ShopperContextService,
+    private reflektion: ReflektionService,
   ) {
   }
 
@@ -34,4 +36,10 @@ export class OCMPreviewSearch implements OnInit {
     this.close.emit(null);
     this.context.router.toProductList({ search: searchText.toLowerCase() })
   }
+
+  async hoverOnSearchPrase(searchPhrase: string) {
+    var userID = this.context.currentUser.isAnonymous() ? null : this.context.currentUser.get().ID;
+    var data = await this.reflektion.searchPreviewProducts(searchPhrase, userID);
+    this.results.content = data.content;
+  } 
 }
