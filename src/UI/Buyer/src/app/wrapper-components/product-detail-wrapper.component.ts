f9a74431dbc5796e774095e56652b648a06bbc38
Oliver Heywood <oliver.heywood@sitecore.com>
2021-11-22 09:45:10 -0600

    product detail page links from pdp work

--- src/UI/Buyer/src/app/wrapper-components/product-detail-wrapper.component.ts
+++ src/UI/Buyer/src/app/wrapper-components/product-detail-wrapper.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { SuperHSProduct } from '@ordercloud/headstart-sdk'
 import { CurrentUserService } from '../services/current-user/current-user.service'
 
@@ -12,10 +12,15 @@ export class ProductDetailWrapperComponent implements OnInit {
   product: SuperHSProduct
   constructor(
     private activatedRoute: ActivatedRoute,
-    protected currentUser: CurrentUserService
+    protected currentUser: CurrentUserService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.product = this.activatedRoute.snapshot.data.product
+    this.router.events.subscribe(event => {
+      window.scroll(0,0);
+      this.product = this.activatedRoute.snapshot.data.product
+    })
   }
 }
