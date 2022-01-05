038d4243ee277d27636d0ef4f0f6d6e80d27968f
Rob Earlam <robert.earlam@gmail.com>
2022-01-05 13:36:11 +1100

    Updated variable patching values to get applications running in containers, added ability to control api middleware build configuration

--- docker/build/UI/Buyer.sh
+++ docker/build/UI/Buyer.sh
@@ -2,7 +2,8 @@ json -I -f  /usr/share/nginx/html/assets/appConfigs/defaultbuyer-test.json \
       -e "this.clientID='$BUYER_CLIENT_ID'" \
       -e "this.middlewareUrl='$MIDDLEWARE_URL'" \
       -e "this.sellerID='$SELLER_ID'" \
-      -e "this.translateBlobUrl='$TRANSLATE_BLOB_URL'"
+      -e "this.translateBlobUrl='$TRANSLATE_BLOB_URL'" \
+      -e "this.baseUrl='$BUYER_URL'" 
 
 cd /usr/share/nginx/html
 node inject-appconfig defaultbuyer-test
--- docker/build/UI/Seller.sh
+++ docker/build/UI/Seller.sh
@@ -3,8 +3,7 @@ json -I -f /usr/share/nginx/html/assets/appConfigs/defaultadmin-test.json \
       -e "this.clientID='$SELLER_CLIENT_ID'" \
       -e "this.middlewareUrl='$MIDDLEWARE_URL'" \
       -e "this.translateBlobUrl='$TRANSLATE_BLOB_URL'" \
-      -e "this.blobStorageUrl='$BLOB_STORAGE_URL'" \
-      -e "this.buyerConfigs['Default Buyer'].clientID='$BUYER_CLIENT_ID'"
+      -e "this.blobStorageUrl='$BLOB_STORAGE_URL'"
 
 cd /usr/share/nginx/html
 node inject-appconfig defaultadmin-test
