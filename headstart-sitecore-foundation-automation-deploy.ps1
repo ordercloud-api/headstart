b97970b0672253b62a41f36de5eb68f3536351e8
Pravesh Patel <100225700+ppatel-sitecore@users.noreply.github.com>
2022-03-21 19:54:33 -0700

    WIP Fixing azure-pipelines scripts
    WIP Fixing azure-pipelines scripts

--- headstart-sitecore-foundation-automation-deploy.ps1
+++ headstart-sitecore-foundation-automation-deploy.ps1
@@ -1,18 +1,17 @@
 $RootScriptPath = Get-Location;
 $RootPackagesPath = ("{0}\packages" -f $RootScriptPath);;
-$SitecoreFoundationSitecoreExtensions = ("{0}\src\Middleware\src\SitecoreExtensions\code\" -f $RootScriptPath);
-$SitecoreFoundationSitecoreExtensionsPackages = ("{0}\packages" -f $SitecoreFoundationSitecoreExtensions);
 $MachineName = $env:computername;
 $MSBuildExe = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
 
 function SitecoreFoundation-PackagesSynchronization-Autotmation {
 	try 
 	{
-		if ((Test-Path $RootPackagesPath) -and (Test-Path $SitecoreFoundationSitecoreExtensions))
+		if ((Test-Path $RootPackagesPath))
 		{
-			Write-Host "Synchronization of 'SitecoreFoundationSitecoreExtensionsPackages' to 'RootPackagesPath' - Started";
-			Copy-Item -Path ("{0}\\*" -f $SitecoreFoundationSitecoreExtensions) -Destination $RootPackagesPath -PassThru;
-			Write-Host "Synchronization of 'SitecoreFoundationSitecoreExtensionsPackages' to 'RootPackagesPath'  - Completed";
+			Write-Host "Synchronization of 'RootPackagesPath' for missing packages - Started";
+			cd $SitecoreFoundationSitecoreExtensionsPackages
+			Install-Package Microsoft.CodeDom.Providers.DotNetCompilerPlatform -Version 3.6.0 
+			Write-Host "Synchronization of 'RootPackagesPath' for missing packages  - Completed";
 		}
 		else
 		{
