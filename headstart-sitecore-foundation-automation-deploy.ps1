03131934fc332ed3a6835241d04ce2209b463960
Pravesh Patel <100225700+ppatel-sitecore@users.noreply.github.com>
2022-03-21 19:04:39 -0700

    WIP Fixing azure-pipelines scripts
    WIP Fixing azure-pipelines scripts

--- headstart-sitecore-foundation-automation-deploy.ps1
+++ headstart-sitecore-foundation-automation-deploy.ps1
@@ -1,100 +1,27 @@
 $RootScriptPath = Get-Location;
 $RootPackagesPath = ("{0}\\packages\" -f $RootScriptPath);;
 $SitecoreFoundationSitecoreExtensions = ("{0}\\src\Middleware\src\SitecoreExtensions\code\" -f $RootScriptPath);
+$SitecoreFoundationSitecoreExtensionsPackages = ("{0}\\packages\" -f $SitecoreFoundationSitecoreExtensions);
 $MachineName = $env:computername;
 $MSBuildExe = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
 
-function buildVS
-{
-    param
-    (
-        [parameter(Mandatory=$true)]
-        [String] $path,
-
-        [parameter(Mandatory=$true)]
-        [bool] $nuget = $true,
-        
-        [parameter(Mandatory=$true)]
-        [bool] $clean = $true
-    )
-	
-    process
-    {
-        if ($nuget) {
-            Write-Host "Restoring NuGet packages" -foregroundcolor green
-            nuget restore "$($path)"
-        }
-
-        if ($clean) {
-            Write-Host "Cleaning $($path)" -foregroundcolor green
-            & "$($MSBuildExe)" "$($path)" /t:Clean /m
-        }
-
-        Write-Host "Building $($path)" -foregroundcolor green
-        & "$($MSBuildExe)" "$($path)" /t:Build /m
-    }
-}
-
-function Upgrade-Current-Python-Version {
+function SitecoreFoundation-PackagesSynchronization-Autotmation {
 	try 
 	{
-		Write-Host "Building Upgrade-Current-Python-Version with Dependencies - Started.";
-		$isChocolateyInstalled = powershell choco -v;
-		
-		if (-not($isChocolateyInstalled)) 
+		if ((Test-Path $RootPackagesPath) -and (Test-Path $SitecoreFoundationSitecoreExtensions))
 		{
-			Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-		}
-		else 
-		{
-			Write-Host "Chocolate version {$isChocolateyInstalled} is already installed.";
-		}
-		
-		$python310FolderPath = "C:\Python310";
-		if (-not(Test-Path $python310FolderPath))
-		{				
-			choco install -y --f python --version=3.10.0;
-		}
-		else 
-		{
-			Write-Host "The {$python310FolderPath} folder already exist.";
-		}
-		Write-Host "Building Upgrade-Current-Python-Version with Dependencies - Completed.";
-	}
-	catch 
-	{
-		Quit ("An Exception error occured in the Upgrade-Current-Python-Version method. {0}>" -f $Error[0]);
-	}
-}
-
-function SitecoreFoundation-Initialization-Autotmation {
-	try 
-	{
-		if ((Test-Path $RootPackagesPath))
-		{
-			cd $RootPackagesPath
-			Install-Package Microsoft.CodeDom.Providers.DotNetCompilerPlatform -Version 3.6.0
-		}
-		if ((Test-Path $SitecoreFoundationSitecoreExtensions))
-		{
-			Write-Host "Building Headstart Repo Solution with Dependencies - Started";
-			if ((Test-Path $MSBuildExe))
-			{
-				Write-Host "Visual Studio Build Tools 2019 already installed and configured ('$MSBuildExe')";
-			}
-			
-			cd $SitecoreFoundationSitecoreExtensions;
-			buildVS Sitecore.Foundation.SitecoreExtensions.sln $true $true;
-			Write-Host "Building Headstart Repo Solution with Dependencies - Completed";
+			Write-Host "Synchronization of 'SitecoreFoundationSitecoreExtensionsPackages' to 'RootPackagesPath' - Started";
+			Copy-Item -Path $SitecoreFoundationSitecoreExtensionsPackages -Force -Recurse -Destination $RootPackagesPath -Force -Recurse
+			Write-Host "Synchronization of 'SitecoreFoundationSitecoreExtensionsPackages' to 'RootPackagesPath'  - Completed";
 		}
 		else
 		{
-			Write-Host "One of the following folders: '$SitecoreFoundationSitecoreExtensions' does not exist.";	
+			Write-Host "One of the following folders: '$SitecoreFoundationSitecoreExtensionsPackages'; '$RootPackagesPath' does not exist.";	
 		}
 	}
 	catch 
 	{
-		Quit ("An Exception error occured in the SitecoreFoundation-Initialization-Autotmation. {0}" -f $Error[0]);
+		Quit ("An Exception error occured in the SitecoreFoundation-PackagesSynchronization-Autotmation. {0}" -f $Error[0]);
 	}
 }
 
@@ -113,9 +40,8 @@ function Quit($Text) {
     Break Script;
 }
 
-Write-Host "SitecoreFoundation-Initialization-Autotmation - Started";
+Write-Host "SitecoreFoundation-PackagesSynchronization-Autotmation - Started";
 Set-ItemProperty 'HKLM:\System\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -value 1 -Force;
-#Upgrade-Current-Python-Version;
-#SitecoreFoundation-Initialization-Autotmation;
-Write-Host "SitecoreFoundation-Initialization-Autotmation - Completed";
+SitecoreFoundation-Initialization-Autotmation;
+Write-Host "SitecoreFoundation-PackagesSynchronization-Autotmation - Completed";
 cd $RootScriptPath;
