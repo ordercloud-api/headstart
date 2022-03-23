fa67567106c68e42211a829d57779270b440a744
Pravesh Patel <prap@sitecore.net>
2022-03-23 09:28:36 -0700

    Added fixes for restoring dependent Sitecore NuGet packages.
    1. Final: Added fixes for restoring dependent Sitecore NuGet packages.

--- headstart-sitecore-foundation-automation-deploy.ps1
+++ headstart-sitecore-foundation-automation-deploy.ps1
@@ -31,13 +31,52 @@ function SitecoreFoundation-PackagesSynchronization-Autotmation {
 	}
 }
 
-function GetBranch($Branch, $Repo) {
-	git checkout $Branch;
-	git fetch;
-	git clean -f;
-	git reset --hard;
-	git pull;
-	Write-Host "The branch: $Branch was selected for the $Repo repo.";
+function GetDotNet4FrameworkVersion($releaseKey) 
+{
+	$releaseVersion = "";
+	
+	if ($releaseKey >= 528040) 
+	{
+		$releaseVersion = "4.8";
+	}
+    if ($releaseKey >= 461808)
+	{
+		$releaseVersion = "4.7.2";
+	}
+    if ($releaseKey >= 461308)
+	{
+		$releaseVersion = "4.7.1";
+	}
+    if ($releaseKey >= 460798)
+	{
+		$releaseVersion = "4.7";
+	}
+    if ($releaseKey >= 394802)
+	{
+		$releaseVersion = "4.6.2";
+	}
+    if ($releaseKey >= 394254)
+	{
+		$releaseVersion = "4.6.1";
+	}
+    if ($releaseKey >= 393295)
+	{
+		$releaseVersion = "4.6";
+	}
+    if ($releaseKey >= 379893)
+	{
+		$releaseVersion = "4.5.2";
+	}
+    if ($releaseKey >= 378675)
+	{
+		$releaseVersion = "4.5.1";
+	}
+    if ($releaseKey >= 378389)
+	{
+		$releaseVersion = "4.5";
+	}
+    Write-Host "The current .NET 4 version installed is '$releaseVersion'.";
+	return $releaseVersion;
 }
 
 function Quit($Text) {
@@ -48,8 +87,12 @@ function Quit($Text) {
 
 Write-Host "SitecoreFoundation-PackagesSynchronization-Autotmation - Started";
 Set-ItemProperty 'HKLM:\System\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -value 1 -Force;
-choco install dotnetfx;
-choco install netfx-4.8-devpack;
+$releaseKey = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release;
+if (GetDotNet4FrameworkVersion($releaseKey) -ne "4.8") 
+{
+	choco install dotnetfx;
+	choco install netfx-4.8-devpack;
+}
 SitecoreFoundation-PackagesSynchronization-Autotmation;
 Write-Host "SitecoreFoundation-PackagesSynchronization-Autotmation - Completed";
 cd $RootScriptPath;
