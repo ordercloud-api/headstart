7bd76f17cc89fcab3dad0712ed062426496a3c3b
Andrew Sutherland <andrew.sutherland@live.com>
2022-05-17 13:06:14 +1000

    - Removed FreightPop integration project

--- src/Middleware/integrations/ordercloud.integrations.freightpop/Models/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ordercloud.integrations.freightpop
-{
-    public class Response<TData>
-    {
-        public TData Data { get; set; }
-        public int Code { get; set; }
-        public string Message { get; set; }
-    }
-}
