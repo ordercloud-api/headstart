ebdd8fbf0d3738098043b98f67fd1c9b8529375b
Crhistian <cramirez@four51.com>
2022-05-18 16:27:02 -0500

    remove unused package-lock.json at the root of the project

--- src/Middleware/src/Headstart.Common/AppSettings.cs
+++ src/Middleware/src/Headstart.Common/AppSettings.cs
@@ -122,6 +122,10 @@ namespace Headstart.Common
     {
         public string ApiUrl { get; set; }
 
+        public string ClientIDsWithAPIAccess { get; set; } // Comma-separated list
+
+        public string IncrementorPrefix { get; set; }
+
         public string MiddlewareClientID { get; set; }
 
         public string MiddlewareClientSecret { get; set; }
@@ -131,40 +135,35 @@ namespace Headstart.Common
         public string MarketplaceName { get; set; } // used for display purposes
 
         public string WebhookHashKey { get; set; }
-
-        public string IncrementorPrefix { get; set; }
-
-        // Comma-separated list
-        public string ClientIDsWithAPIAccess { get; set; }
     }
 
     public class SendgridSettings
     {
         public string ApiKey { get; set; }
 
-        public string FromEmail { get; set; }
+        public string BillingEmail { get; set; } // (Optional) Email to send for payment, billing, or refund queries
 
         public string CriticalSupportEmails { get; set; } // (Optional) Comma delimited list of emails that should be contacted when criticial failures occur that require manual intervention
 
-        public string SupportCaseEmail { get; set; } // (Optional) Email to send support cases to
+        public string CriticalSupportTemplateID { get; set; } // (Optional but required to send CriticalSupport emails) ID for template to be used for CriticalSupport emails
 
-        public string BillingEmail { get; set; } // (Optional) Email to send for payment, billing, or refund queries
-
-        public string OrderSubmitTemplateID { get; set; } // (Optional but required to send OrderSubmit emails) ID for the template to be used for OrderSubmit emails
-
-        public string OrderApprovalTemplateID { get; set; } // (Optional but required to send OrderApproval emails) ID for template to be used for OrderApproval emails
+        public string FromEmail { get; set; }
 
         public string LineItemStatusChangeTemplateID { get; set; } // (Optional but required to send LineItemStatusChange emails) ID for template to be used for LineItemStatusChange emails
 
-        public string QuoteOrderSubmitTemplateID { get; set; } // (Optional but required to send QuoteOrderSubmit emails) ID for template to be used for QuoteOrderSubmit emails
-
         public string NewUserTemplateID { get; set; } // (Optional but required to send NewUser emails) ID for template to be used for NewUser emails
 
-        public string ProductInformationRequestTemplateID { get; set; } // (Optional but required to send ProductInformationRequest emails) ID for template to be used for ProductInformationRequest emails
+        public string OrderApprovalTemplateID { get; set; } // (Optional but required to send OrderApproval emails) ID for template to be used for OrderApproval emails
+
+        public string OrderSubmitTemplateID { get; set; } // (Optional but required to send OrderSubmit emails) ID for the template to be used for OrderSubmit emails
 
         public string PasswordResetTemplateID { get; set; } // (Optional but required to send PasswordReset emails) ID for template to be used for PasswordReset emails
 
-        public string CriticalSupportTemplateID { get; set; } // (Optional but required to send CriticalSupport emails) ID for template to be used for CriticalSupport emails
+        public string ProductInformationRequestTemplateID { get; set; } // (Optional but required to send ProductInformationRequest emails) ID for template to be used for ProductInformationRequest emails
+
+        public string QuoteOrderSubmitTemplateID { get; set; } // (Optional but required to send QuoteOrderSubmit emails) ID for template to be used for QuoteOrderSubmit emails
+
+        public string SupportCaseEmail { get; set; } // (Optional) Email to send support cases to
     }
 
     public class ServiceBusSettings
