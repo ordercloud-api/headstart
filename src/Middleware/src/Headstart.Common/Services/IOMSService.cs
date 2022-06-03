c45fea466761e297d6a936a69ee629fff3812ffb
Andrew Sutherland <andrew.sutherland@live.com>
2022-06-03 17:34:20 +1000

    Adding new classes omitted from previous re-architecture progress commit.

---
+++ src/Middleware/src/Headstart.Common/Services/IOMSService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Headstart.Common.Models;
+using Headstart.Models;
+using Headstart.Models.Headstart;
+
+namespace Headstart.Common.Services
+{
+    public interface IOMSService
+    {
+        Task<ProcessResult> ExportOrder(HSOrderWorksheet worksheet, IList<HSOrder> supplierOrders, bool isOrderSubmit = true);
+    }
+}
