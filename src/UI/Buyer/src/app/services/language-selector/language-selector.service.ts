58f9381ac5ffad374df1e339316b5af7753974a5
Crhistian <cramirez@four51.com>
2022-06-22 12:58:47 -0500

    Fixes bug where cookie not set when language defined implicitly (Buyer)

--- src/UI/Buyer/src/app/services/language-selector/language-selector.service.ts
+++ src/UI/Buyer/src/app/services/language-selector/language-selector.service.ts
@@ -43,20 +43,18 @@ export class LanguageSelectorService {
       }
     }
 
-    this.cookieService.putObject(this.languageCookieName, language)
+    this.cookieService.put(this.languageCookieName, language)
     this.SetTranslateLanguage()
   }
 
   /**
    * Implicitly sets the language used by the translate service
    */
-  public async SetTranslateLanguage(): Promise<void> {
+  public SetTranslateLanguage(): void {
     const browserCultureLang = this.translate.getBrowserCultureLang()
     const browserLang = this.translate.getBrowserLang()
     const languages = this.translate.getLangs()
-    const selectedLang = this.cookieService
-      .getObject(this.languageCookieName)
-      ?.toString()
+    const selectedLang = this.cookieService.get(this.languageCookieName)
     const isAnonymousUser = this.tokenHelper.isTokenAnonymous()
     let xpLang
 
@@ -65,19 +63,24 @@ export class LanguageSelectorService {
       xpLang = user?.xp?.Language
     }
     if (xpLang) {
-      this.translate.use(xpLang)
+      this.useLanguage(xpLang)
     } else if (selectedLang && languages.includes(selectedLang)) {
-      this.translate.use(selectedLang)
+      this.useLanguage(selectedLang)
     } else if (languages.includes(browserCultureLang)) {
-      this.translate.use(browserCultureLang)
+      this.useLanguage(browserCultureLang)
     } else if (languages.includes(browserLang)) {
-      this.translate.use(browserLang)
+      this.useLanguage(browserLang)
     } else if (languages.includes(this.appConfig.defaultLanguage)) {
-      this.translate.use(this.appConfig.defaultLanguage)
+      this.useLanguage(this.appConfig.defaultLanguage)
     } else if (languages.length > 0) {
-      this.translate.use(languages[0])
+      this.useLanguage(languages[0])
     } else {
       throw new Error('Cannot identify a language to use.')
     }
+  }
+
+  private useLanguage(language: string) {
+    this.translate.use(language)
+    this.cookieService.put(this.languageCookieName, language)
   }
 }
