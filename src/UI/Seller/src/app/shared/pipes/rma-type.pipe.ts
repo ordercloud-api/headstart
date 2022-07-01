aaa08e24585cd6caf68c454273832522704f8fa4
Crhistian <cramirez@four51.com>
2022-07-01 11:22:24 -0500

    remove debugger

--- src/UI/Seller/src/app/shared/pipes/rma-type.pipe.ts
+++ src/UI/Seller/src/app/shared/pipes/rma-type.pipe.ts
@@ -5,7 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core'
 })
 export class RmaTypePipe implements PipeTransform {
   transform(value: string): string {
-    debugger
     if (value === 'Cancellation') {
       return 'ADMIN.RMAS.CANCELLATION'
     } else if (value === 'Return') {
