1f2bff58ce38d0c7fd0044a44d4c40aec267b371
Crhistian <cramirez@four51.com>
2022-12-29 11:08:04 -0600

    Resolve issues preventing app to build after package updates

--- src/UI/Seller/src/app/app.module.ts
+++ src/UI/Seller/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { HttpClientModule, HttpClient } from '@angular/common/http'
 // 3rd party
 import { CookieModule } from 'ngx-cookie'
 import { ToastrModule } from 'ngx-toastr'
-import { NgProgressModule } from '@ngx-progressbar'
-import { NgProgressHttpModule } from '@ngx-progressbar/http'
+import { NgProgressModule } from 'ngx-progressbar'
+import { NgProgressHttpModule } from 'ngx-progressbar/http'
 import {
   TranslateModule,
   TranslateLoader,
