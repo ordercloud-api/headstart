71c44ba406ac824174819d5580939dab62abb9b6
Crhistian <cramirez@four51.com>
2023-02-27 11:23:49 -0600

    Remove all test dependencies for buyer (not being used)

--- src/UI/Buyer/src/app/components/products/spec-form/spec-field.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SpecFieldDirective } from './spec-field.directive';
-
-describe('SpecFieldDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SpecFieldDirective();
-    expect(directive).toBeTruthy();
-  });
-});
